21fc2887e7362098d95c9f532e5023489a94a4f2
Bach <wantsomelemonade04@gmail.com>
2024-10-30 11:28:35 +0700

    not thing much

---
+++ src/main/java/com/example/personalfinance/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.example.personalfinance.controller;
+
+import com.example.personalfinance.repository.UserRepository;
+import com.example.personalfinance.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class UserController {
+  private final UserService userService;
+  private final UserRepository userRepository;
+  private final JWT
+
+}
